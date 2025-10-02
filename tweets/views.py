0dedb9518f6596dd4e11f4e12c5b8b8829770f37
Shar221 <sharlinenm2020@gmail.com>
2025-10-02 22:26:24 +0300

    views and url routing

--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from .models import Tweet
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import UsercreationForm
+from django.contrib import messages
 
-# Create your views here.
+def home(request):
+    tweets = Tweet.objects.all().order_by('-created_at')
+    return render(request, 'tweets/home.html', {'tweets': tweets})
+
+@login_required
+def create_tweet(request):
+    if request.method == 'POST':
+        content = request.POST.get('content')
+        if content:
+            Tweet.objects.create(user=request.user, content=content)
+            messages.success(request, 'Tweet created successfully!')
+        else:
+            messages.error(request, 'Content cannot be empty.')
+    return render(request, 'tweets/create_tweet.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Account created successfully! You can now log in.')
+            return redirect('login')
+        else:
+            messages.error(request, 'Please correct the error below.')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/signup.html', {'form': form})
+
